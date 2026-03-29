1c5a23dc6cdb58b75139bdf360bfa06d7e7b9c9e
Hugo Thomas Groussard <hugothomasgroussard@Mac.lan>
2026-03-30 00:50:35 +0200

    Add favicon

--- app/layout.js
+++ app/layout.js
@@ -38,6 +38,8 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Crafto" />
